b9ccab89ef410d1a40fe19b032d47a898c6ea0ad
Vaishali Gupta <Vaishali@codeaurora.org>
2019-10-18 14:25:30 -0700

    Revert "wifi: Remove dynamic interfaces as part of wifi chip stop/cleanup."
    This reverts commit d040bf14ce9abfe56bebd94d4a304e10b1c009a9.
    
    Change-Id: Ibd5c25b28fc8790a24242b5fd7dec81441a04d6b

--- wifi/1.3/default/wifi_chip.h
+++ wifi/1.3/default/wifi_chip.h
@@ -246,7 +246,6 @@ class WifiChip : public V1_3::IWifiChip {
     std::string allocateApIfaceName();
     std::string allocateStaIfaceName();
     bool writeRingbufferFilesInternal();
-    void QcRemoveAndClearDynamicIfaces();
 
     ChipId chip_id_;
     std::weak_ptr<legacy_hal::WifiLegacyHal> legacy_hal_;
